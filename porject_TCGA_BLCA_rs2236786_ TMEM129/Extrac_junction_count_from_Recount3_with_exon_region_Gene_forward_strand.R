a9009bc724fa328f4ca189e1ca502277dde092e2
ChiaHan-1215 <chiahan.lee@nih.gov>
2025-11-05 11:37:23 -0500

    Update and rename Extrac_junction_count_from_Recount3_with_exon_region.R to Extrac_junction_count_from_Recount3_with_exon_region_Gene_forward_strand.R

--- porject_TCGA_BLCA_rs2236786_ TMEM129/Extrac_junction_count_from_Recount3_with_exon_region_Gene_forward_strand.R
+++ porject_TCGA_BLCA_rs2236786_ TMEM129/Extrac_junction_count_from_Recount3_with_exon_region_Gene_forward_strand.R
@@ -1,4 +1,5 @@
-# Goal: extract junction count from Recount3 dataset. Target: TMEM129 and FGFR3 
+# Goal: extract junction count from Recount3 dataset. Target: TACC3 and FGFR3 
+# Note: For this code is for gene that start from 5' to 3' -> direction. The gene from 3' to 5' is in other script 
 
 library(recount3)
 library(snapcount)
@@ -60,9 +61,9 @@ df_chr4$strand <- gsub('chr4:[0-9]+-[0-9]+:','',rownames(df_chr4))
 df_chr4$start <- as.numeric(df_chr4$start)
 df_chr4$end <- as.numeric(df_chr4$end)
 
-df_chr4 <- df_chr4 %>% 
-  filter(start >= 1792585 & start <= 1809586 & 
-           end >= 1792585 & end <= 1809586)
+# df_chr4 <- df_chr4 %>% 
+#   filter(start >= 1792585 & start <= 1809586 & 
+#            end >= 1792585 & end <= 1809586)
 
 
 df_chr4 <- df_chr4[,c('chr','start','end','strand',grep("TCGA",names(df_chr4),value = T))]
@@ -77,8 +78,18 @@ df_chr4 <- df_chr4[,c(grep("location",names(df_chr4),value = T),grep("TCGA",name
 library(rtracklayer)
 library(dplyr)
 
-gtf <- import('gencode.v39.annotation.gtf.gz')      # Use your downloaded GTF file
-genes_of_interest <- c("FGFR3")    # Replace with your gene list
+gtf <- import('gencode.v39.annotation.gtf.gz')     
+
+# c("FGFR3")
+
+# 5 to 3 ->
+genes_of_interest <-c("TACC3","FGFR3")
+# 3 to 5 <-
+# genes_of_interest <-c("FAM53A","SLBP","TMEM129")
+
+
+# Since there's difference of 5' to 3' gene and 3' to 5' gene 
+# so need to seperated by gene direct, the +1 -1 base for this code is for FGFR3,TACC3
 
 # Subset exons for your genes
 exons <- subset(gtf, type == "exon" & gene_name %in% genes_of_interest)
@@ -89,18 +100,16 @@ exon_df <- as.data.frame(exons)
 exon_df <- exon_df[, c("seqnames", "start", "end", "gene_name","transcript_name", "exon_id", "exon_number")]
 exon_df$location <- paste0(exon_df$seqnames,':',exon_df$start,"-",exon_df$end)
 
-exon_df.u <- exon_df %>% filter(transcript_name == "FGFR3-205")
-
 # exon_df.u <- exon_df[!duplicated(exon_df[, c("exon_id", "location")]), ]
 
-FGFR_iso <- exon_df$transcript_name %>% unique() %>% sort()
+iso_list <- exon_df$transcript_name %>% unique() %>% sort()
 
 jct_per_iso <- data.frame()
 
 
-for (k in FGFR_iso){
+for (k in iso_list){
   
-  # k <- FGFR_iso[2]
+  # k <- iso_list[2]
   exon_df.u <- exon_df %>% filter(transcript_name == k)
   
   jct_df <- exon_df.u %>%
@@ -113,6 +122,7 @@ for (k in FGFR_iso){
     filter(!is.na(next_start), as.numeric(next_exon) == as.numeric(exon_number) + 1) %>%
     ungroup() %>%    
     transmute(
+      gene_name,
       transcript_name,
       jct_label = paste0("jct_ex", exon_number, "_ex", next_exon),
       location = paste0(seqnames, ":", end + 1L,"-", next_start - 1L)
@@ -131,16 +141,20 @@ df_final <- left_join(jct_per_iso,df_chr4,by='location')
 # remove NA
 df_final <- na.omit(df_final)
 
+# Now save seperated by gene
+df_final$gene_name %>% unique()
 
-###################################
+df_final.s <- df_final %>% filter(gene_name == "TACC3")
 
-df_final <- df_final[,-c(1,2)]
-df_final <- df_final[order(df_final$location),]
-df_final.t <- as.data.frame(t(df_final))
-names(df_final.t) <- df_final.t[1,]
-df_final.t <- df_final.t[-1,]
-df_final.t[] <- lapply(df_final.t[], as.numeric)
-df_final.t <- df_final.t %>% mutate(TCGA_ID=rownames(df_final.t),.before = 1)
+df_final.s <- df_final.s[,-c(1:3)]
+df_final.s <- df_final.s[order(df_final.s$location),]
+df_final.s.t <- as.data.frame(t(df_final.s))
+names(df_final.s.t) <- df_final.s.t[1,]
+df_final.s.t <- df_final.s.t[-1,]
+df_final.s.t[] <- lapply(df_final.s.t[], as.numeric)
+df_final.s.t <- df_final.s.t %>% mutate(TCGA_ID=rownames(df_final.s.t),.before = 1)
 
-# write.table(df_chr4.t,paste0("~/Desktop/chr15_CHRNA5_bladder_project/Recount3_for_GTEx_and_TCGA/TCGA_JC/",i,"_C5iso_jc.csv"),col.names = T,row.names = F,sep = ',',quote = F)
+write.table(df_final.s.t,paste0("Recount_result/TCGA_BLCA_TACC3_jct_from_Recount3.csv"),col.names = T,row.names = F,sep = ',',quote = F)
 
+rm(df_final.s)
+rm(df_final.s.t)
